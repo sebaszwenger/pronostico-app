e77126a8e0266dfe5127313ba5c622c7689a2279
sebaszwenger <sebaszwenger@gmail.com>
2022-05-31 01:03:00 -0300

    I corrected the data of the time of registration of the weather

--- src/components/weatherCard/HeaderCard.js
+++ src/components/weatherCard/HeaderCard.js
@@ -2,12 +2,12 @@ import { useContext } from "react";
 import DataContext from "../../hooks/DataContext";
 
 const HeaderCard = () => {
-  const { location } = useContext(DataContext);
+  const { location, current } = useContext(DataContext);
   return (
     <h2 className="bg-slate-800 opacity-70 rounded-t-md py-2 px-5 text-white text-2xl font-bold mx-3">
       {location.name}, {location.region}
       <span className="ml-2 text-sm">
-        Hasta {location.localtime.substring(11)}
+        Registrado a las: {current.last_updated.substring(11)}
       </span>
     </h2>
   );
