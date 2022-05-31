b51e72bffdeec07c41004cd992657d19febaaaf5
sebaszwenger <sebaszwenger@gmail.com>
2022-05-31 00:33:27 -0300

    add the routing to the different pages and create a state with the array of the favorite cities

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BookmarkContext from "./hooks/BookmarkContext";
+import Home from "./pages/Home";
+import NoMatch from "./pages/NoMatch";
+import Search from "./pages/Search";
+
 function App() {
+  const [bookmark, setBookmark] = useState([]);
+
   return (
-    <div className="App">
-      <h1>pronostico app</h1>
-    </div>
+    <BrowserRouter>
+      <BookmarkContext.Provider value={[bookmark, setBookmark]}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search/:id" element={<Search />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </BookmarkContext.Provider>
+    </BrowserRouter>
   );
 }
 
