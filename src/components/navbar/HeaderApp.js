c2d32512e3eb30b25cd79597920aa8057d5dbf00
sebaszwenger <sebaszwenger@gmail.com>
2022-05-30 21:17:23 -0300

    I created the HeaderApp component that renders the app's headline

---
+++ src/components/navbar/HeaderApp.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const HeaderApp = () => {
+  return (
+    <div className="flex justify-center">
+      <Link to="/">
+        <h1 className="w-fit hover:scale-105  text-5xl  font-extrabold mt-5 mb-7 text-transparent bg-clip-text bg-gradient-to-r from-red-700 via-yellow-700 to-yellow-500 ">
+          Pronostico App
+        </h1>
+      </Link>
+    </div>
+  );
+};
+
+export default HeaderApp;
